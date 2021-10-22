3b06b787e9d725346de428430fa55fa937d3ca02
bor-nikkk <bor-nikkk@mail.ru>
2021-10-22 11:48:40 +0300

    4

--- app/src/main/java/com/bornikkk/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/bornikkk/myfirstapp/MainActivity.kt
@@ -2,11 +2,31 @@ package com.bornikkk.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
-    override  fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //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
+        R.color.purple_200
+
+        val helloTextYiew: TextView = findViewById(R.id.hello_text_view)
+        val randomButton: Button = findViewById(R.id.button)
+
+        helloTextYiew.text = "Hello World123"
+
+        randomButton.setOnClickListener {
+            val randVal = Random.nextInt(100)
+
+            helloTextYiew.text = randVal.toString()
+        }
+
+
+
+
+        //ctrl + alt + shift + L
     }
 }
