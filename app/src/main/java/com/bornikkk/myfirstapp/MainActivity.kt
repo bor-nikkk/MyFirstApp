65b2cda2247e2ca456bf6702bf6ae8399dd9f3e9
bor-nikkk <bor-nikkk@mail.ru>
2021-10-22 10:50:47 +0300

    2

--- app/src/main/java/com/bornikkk/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/bornikkk/myfirstapp/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override  fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //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
     }
 }
